670e1acfe51549337308e84bf6dd4e7fe5da27a3
Filip Tenerowicz <142356613+eXegiMonumentum@users.noreply.github.com>
2025-03-05 17:15:20 +0100

    Update cats_api_project.py

--- cats_api_project.py
+++ cats_api_project.py
@@ -1,168 +1,112 @@
 import json
 import webbrowser
-from pprint import pprint
 import requests
-import credentials
+import os
+from pprint import pprint
 
+# Importowanie lub tworzenie pliku credentials
+if not os.path.exists("credentials.py"):
+    api_key = input("Podaj swój API Key: ")
+    with open("credentials.py", "w") as f:
+        f.write(f'API_KEY = "{api_key}"\n')
+        f.write('headers = {"x-api-key": API_KEY}\n')
+
+import credentials
 
 def load_json_to_python_format(response):
     try:
-        content = response.json()
+        return response.json()
     except json.decoder.JSONDecodeError:
-        print("bad format" , response.text)
-    else:
-        return content
+        print("Bad format:", response.text)
+        return None
 
 def get_cats_breeds_description():
-    r = requests.get("https://api.thecatapi.com/v1/breeds")
+    r = requests.get("https://api.thecatapi.com/v1/breeds", headers=credentials.headers)
     return load_json_to_python_format(r)
 
-def cats_breeds_id_list(breedsDescription):
-    catsBreedsIdList  = [
-        line["id"]
-        for line in breedsDescription
-        ]
-    return  catsBreedsIdList
+def cats_breeds_id_list(breeds_description):
+    return [breed["id"] for breed in breeds_description]
 
-def create_breeds_names_list():
-    return [
-        BreedName["name"]
-        for BreedName in breedsDescription
-        ]
+def create_breeds_names_list(breeds_description):
+    return [breed["name"] for breed in breeds_description]
 
-def create_cat_breeds_id_dictionary(catsBreedsIdList):
-    return {
-        number : catsBreedsIdList[number]
-        for number in range(len(catsBreedsIdList))
-        }
-    
-def create_cat_breeds_names_dictionary(breedsNamesList):
-    return {
-    breed : breedsNamesList[breed]
-    for breed in range(len(catsBreedsIdList))
-    }
-  
-def show_favorite_cats_assigned_to_sub_id (userId):
-    params = {
-        "sub_id" : userId
-        }
-    r = requests.get("https://api.thecatapi.com/v1/favourites/", params=params,
-                     headers=credentials.headers)
+def create_cat_breeds_id_dictionary(cats_breeds_id_list):
+    return {i: cats_breeds_id_list[i] for i in range(len(cats_breeds_id_list))}
+
+def create_cat_breeds_names_dictionary(breeds_names_list):
+    return {i: breeds_names_list[i] for i in range(len(breeds_names_list))}
+
+def show_favorite_cats(user_id):
+    r = requests.get("https://api.thecatapi.com/v1/favourites", 
+                     params={"sub_id": user_id}, headers=credentials.headers)
     return load_json_to_python_format(r)
 
-def draw_random_cat_full_picture():
-        r = requests.get("https://api.thecatapi.com/v1/images/search?size=full",
-                         headers=credentials.headers)
-        return load_json_to_python_format(r)[0]
-
-def draw_random_cat_with_chosen_breed_full_picture(selectedNumber):
-    params = {
-        "breed_ids" : sequenceCatBreedsIdDictionary[selectedNumber]
-        }
-    r = requests.get("https://api.thecatapi.com/v1/images/search?size=full",params=params,
-                     headers=credentials.headers)
+def draw_random_cat():
+    r = requests.get("https://api.thecatapi.com/v1/images/search?size=full", headers=credentials.headers)
     return load_json_to_python_format(r)[0]
 
-def add_favourite_cat_to_favourites(catId, userId):
-    data = {
-        "image_id" : catId,
-        "sub_id" : userId
-        }
-    r = requests.post("https://api.thecatapi.com/v1/favourites", json=data,
-                      headers=credentials.headers)
+def draw_random_cat_by_breed(selected_number, breeds_dict):
+    params = {"breed_ids": breeds_dict[selected_number]}
+    r = requests.get("https://api.thecatapi.com/v1/images/search?size=full", params=params, headers=credentials.headers)
+    return load_json_to_python_format(r)[0]
+
+def add_favourite_cat(cat_id, user_id):
+    data = {"image_id": cat_id, "sub_id": user_id}
+    r = requests.post("https://api.thecatapi.com/v1/favourites", json=data, headers=credentials.headers)
     return load_json_to_python_format(r)
 
-def open_new_webbrowser_tab(openUrl, yourCat):
-    if openUrl.upper() == ("Y"):
-        webbrowser.open_new_tab(yourCat["url"])
-    else:
-        print("no open")
-
-def remove_cat_from_favourites(userId, favouriteCatId):
-   
-    r = requests.delete("https://api.thecatapi.com/v1/favourites/"+favouriteCatId,
-                      headers=credentials.headers)
-
+def remove_cat_from_favourites(favourite_cat_id):
+    r = requests.delete(f"https://api.thecatapi.com/v1/favourites/{favourite_cat_id}", headers=credentials.headers)
     return load_json_to_python_format(r)
 
-def check_result_from_remove_favourite_cat(resultFromRemoveFavouriteCat):
-    try:
-        if (resultFromRemoveFavouriteCat["message"] == "SUCCESS"):
-            print("cat removed")
-    except TypeError:
-        print("You wrote wrong id")
+def open_webbrowser_tab(open_url, cat):
+    if open_url.upper() == "Y":
+        webbrowser.open_new_tab(cat["url"])
 
-print("----------------------------------------")    
-userId = "einawomaragorp_ąjsap_ąjom"
-userName = "uczeń"
+def main():
+    print("----------------------------------------")
+    user_id = "einawomaragorp_ąjsap_ąjom"
+    user_name = "uczeń"
+    print(f"Hello {user_name}!")
+    print("This game allows you to find the best cat picture you've ever seen!")
+
+    favourite_cats = show_favorite_cats(user_id)
+    print("Here are your favorite cat pictures:")
+    pprint(favourite_cats)
+    print("----------------------------------------")
+
+    breeds_description = get_cats_breeds_description()
+    if not breeds_description:
+        print("Error loading breeds.")
+        return
     
-print("Hello " ,userName, "!")
-print("""This game allows you to find best picture of cat you have ever seen!
-here you can draw pictures, then added your best choises to your favourites !""")
+    cats_breeds_id_list = cats_breeds_id_list(breeds_description)
+    breeds_names_list = create_breeds_names_list(breeds_description)
+    breeds_id_dict = create_cat_breeds_id_dictionary(cats_breeds_id_list)
+    breeds_names_dict = create_cat_breeds_names_dictionary(breeds_names_list)
 
-favouriteCats = show_favorite_cats_assigned_to_sub_id(userId) #ulubione koty 
-print(" Here are your favourite cat pictures:")
-pprint(favouriteCats)
-print(" ----------------------------------------")
+    amount_of_drawn_cats = int(input("Draw cats. Enter amount (1-5): "))
+    while amount_of_drawn_cats < 1 or amount_of_drawn_cats > 5:
+        amount_of_drawn_cats = int(input("Enter a valid number (1-5): "))
+    
+    breed_choice = input("Would you like to select the cat breed? Y/N ")
+    
+    if breed_choice.upper() == "Y":
+        pprint(breeds_names_dict)
+        selected_number = int(input("Write number: "))
+        for _ in range(amount_of_drawn_cats):
+            cat = draw_random_cat_by_breed(selected_number, breeds_id_dict)
+            print(f"Breed: {breeds_names_list[selected_number]} {cat['url']}")
+            open_webbrowser_tab(input("Open in browser? Y/N: "), cat)
+            if input("Add to favourites? Y/N: ").upper() == "Y":
+                add_favourite_cat(cat["id"], user_id)
+    else:
+        for _ in range(amount_of_drawn_cats):
+            cat = draw_random_cat()
+            print(cat["url"])
+            open_webbrowser_tab(input("Open in browser? Y/N: "), cat)
+            if input("Add to favourites? Y/N: ").upper() == "Y":
+                add_favourite_cat(cat["id"], user_id)
 
-breedsDescription = get_cats_breeds_description() # opis ras
-catsBreedsIdList = cats_breeds_id_list(breedsDescription)   # lista id ras
-breedsNamesList = create_breeds_names_list()                # lista nazw ras
-
-amountOfDrawnCats = int(input("""Draw cats.
-Enter here amount of images you want to draw:
-min 1: max 5: """))
-while (amountOfDrawnCats < 1 or amountOfDrawnCats > 5):    
-    amountOfDrawnCats = int(input("min 1: max 5 "))
-
-sequenceCatBreedsIdDictionary = create_cat_breeds_id_dictionary(catsBreedsIdList) # do numerków  przypisuję id rasy
-sequenceCatBreedsNamesDictionary = create_cat_breeds_names_dictionary(breedsNamesList)
-
-breedsChose = input(" Would you like to select the cat breed?  Y/N ")
-newlyAddedCatInfo = {}
-if (breedsChose.upper() == "Y"):
-    print("Select breeds --> write number")
-    pprint(sequenceCatBreedsNamesDictionary) #słownik by wybrać odpowiedni numer rasy.
-    selectedNumber = int(input("Write number "))
-    i = 0
-    while i < amountOfDrawnCats:
-        yourCat = draw_random_cat_with_chosen_breed_full_picture(selectedNumber)
-        print("Breed you Chosen:",breedsNamesList[selectedNumber]," ",yourCat["url"]) 
-        openUrl = input("Do you want to open url? Y/N ")
-        open_new_webbrowser_tab(openUrl, yourCat) 
-        favouriteChose = input(" Would you like to add your selected cat to favourites? Y/N ")
-        if favouriteChose.upper() == ("Y"):
-            resultFromAddingFavouriteCat = add_favourite_cat_to_favourites(yourCat["id"], userId) # otrzymam {'message': 'SUCCESS', 'id': 00000}
-            newlyAddedCatInfo = {resultFromAddingFavouriteCat["id"] : yourCat["url"]}   # słownik id : url
-        else:
-            print("No added")
-        i += 1    
-else:
-    print("Random breed")  
-    i = 0
-    while i < amountOfDrawnCats:
-        yourCat = draw_random_cat_full_picture()
-        pprint(yourCat["url"])
-        openUrl = input("Do you want to open url? Y/N ") 
-        open_new_webbrowser_tab(openUrl, yourCat)  
-        favouriteChose = input(" Would you like to add your selected cat to favourites? Y/N ")
-        if favouriteChose.upper() == ("Y"):
-            resultFromAddingFavouriteCat = add_favourite_cat_to_favourites(yourCat["id"], userId)
-            newlyAddedCatInfo = {resultFromAddingFavouriteCat["id"] : yourCat["url"]}
-        else:
-            print("No added") 
-        i += 1
-        
-print("----------------------------------------")    
-
-            
-favouriteCatsById = {
-    favouriteCat["id"] : favouriteCat["image"]["url"] 
-    for favouriteCat in favouriteCats
-    }
-newlyAddedCatInfo.update(newlyAddedCatInfo)
-pprint(favouriteCatsById)
-favouriteCatId = input("Do you want to remove cat from favourites? Write here id: ")
-resultFromRemoveFavouriteCat = remove_cat_from_favourites(userId, favouriteCatId) # rezultat usunięcia  SUCCESS lub TypeError
-check_result_from_remove_favourite_cat(resultFromRemoveFavouriteCat)
-
+if __name__ == "__main__":
+    main()
